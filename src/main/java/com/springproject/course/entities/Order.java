e2bfb1458c5c7cbd748e763fdcbe107a17f37fc5
ErickMartins153 <121458723+ErickMartins153@users.noreply.github.com>
2023-11-13 14:48:34 -0300

    Order, Instant, ISO 8601

---
+++ src/main/java/com/springproject/course/entities/Order.java
@@ -0,0 +1,66 @@
+package com.springproject.course.entities;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+@Entity
+@Table(name = "tb_order") //Order é uma palavra reservada do SQL, por isso dá erro caso não mudemos o nome
+public class Order implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
+    //annotation para formatar
+    private Instant moment;
+    @ManyToOne //pois um cliente pode ter muitos pedidos
+    @JoinColumn(name = "client_id") // client_id será a foreign key
+    private User client;
+
+    public Order() {}
+
+    public Order(Long id, Instant moment, User client) {
+        this.id = id;
+        this.moment = moment;
+        this.client = client;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public User getClient() {
+        return client;
+    }
+
+    public void setClient(User client) {
+        this.client = client;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Order order = (Order) o;
+        return Objects.equals(id, order.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
--- src/main/java/com/springproject/course/entities/User.java
+++ src/main/java/com/springproject/course/entities/User.java
@@ -1,8 +1,11 @@
 package com.springproject.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -15,6 +18,16 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @JsonIgnore //o user possui Order que possuem users, ao fazer uma requisição o JSON recebido entra num looping
+    //para resolver isso precisamos dar o @JsonIgnore em ao menos uma das classes que participam da associação
+    //de mão dupla, então quando damos um GET para users/1, por exemplo, não recebemos as Orders, para mudar isso
+    //precisariamos remover o JsonIgnore daqui e colocá-lo na classe Order, assim orders/1 não retornaria o cliente
+    //mas ao dar users receberiamos as orders associadas a ela
+    @OneToMany(mappedBy = "client") //O equivalente ao que fizemos na classe Order, 1 usuário terá muitos pedidos
+    //entre parenteses precisamos passar o nome do atributo que tem no outro lado da associação
+    //que está na classe order, no caso private User _client_.
+    private List<Order> orders = new ArrayList<>();
 
     public User() {}
     //por estarmos usando framework é obrigatório criar o construtor vazio
@@ -66,6 +79,10 @@ public class User implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 
     @Override
